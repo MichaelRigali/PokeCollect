f37e0fa8662633fde9a25bc333921e7ef776ff79
derrickjhiggins <derrick.higgins@outlook.com>
2023-11-16 11:05:26 -0800

    display initial db data

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-# Sample Flask application for a BSG database, snapshot of BSG_people
-
 from flask import Flask, render_template, json, redirect
-# from flask_mysqldb import MySQL
+from flask_mysqldb import MySQL
 from flask import request
 import os
 
@@ -15,6 +13,8 @@ app.config["MYSQL_PASSWORD"] = "6560"
 app.config["MYSQL_DB"] = "cs340_higginde"
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
+mysql = MySQL(app)
+
 # Specify the location of the templates folder
 app.template_folder = os.path.abspath('templates')
 
@@ -22,11 +22,32 @@ app.template_folder = os.path.abspath('templates')
 # have homepage route to /people by default for convenience, generally this will be your home route with its own template
 @app.route('/')
 def index():
-    return render_template('/users.html')
 
-@app.route('/users.html')
+    if request.method == "GET":
+        # mySQL query to grab all user data
+        query = "SELECT * FROM Users"
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+
+        # render edit_people page passing our query data and homeworld data to the edit_people template
+        return render_template("users.j2", data=data)
+    return render_template('users.j2')
+
+@app.route('/users', methods=["POST", "GET"])
 def users():
-    return render_template('/users.html')
+
+    # Grab Users data so we send it to our template to display
+    if request.method == "GET":
+        # mySQL query to grab all user data
+        query = "SELECT * FROM Users"
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+
+        # render edit_people page passing our query data and homeworld data to the edit_people template
+        return render_template("users.j2", data=data)
+    return render_template('users.j2')
 
 @app.route('/orders.html')
 def orders():
@@ -43,7 +64,22 @@ def payments():
 @app.route('/pokemoncardspecs.html')
 def pokemoncardspecs():
     return render_template('/pokemoncardspecs.html')
+
+
+####### delete routes #######
+# delete from Users
+@app.route("/delete_user/<int:id>")
+def delete_people(id):
+    # mySQL query to delete the person with our passed id
+    query = "DELETE FROM Users WHERE id = '%s';"
+    cur = mysql.connection.cursor()
+    cur.execute(query, (id,))
+    mysql.connection.commit()
+
+    # redirect back to people page
+    return redirect("/people")
+
 # Listener
 # change the port number if deploying on the flip servers
 if __name__ == "__main__":
-    app.run(port=41022, debug=True)
+    app.run(port=41025, debug=True)
